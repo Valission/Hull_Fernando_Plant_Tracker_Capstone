6de79b539240497b608ef48e1340a72520e923b5
Valission <FernandomvHull@gmail.com>
2025-07-14 11:06:57 -0400

    successful fetch

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,14 @@
 import express from 'express'
+import cors from 'cors'
 
 const app = express()
 
 const port = 8080
 
+app.use(cors())
+
 app.get('/', (req, res) =>{
-    res.send('Hello (from server)')
+    res.json('Hello (from server)')
 })
 
 app.listen(port, () => console.log(`Listening on port: ${port}`))
